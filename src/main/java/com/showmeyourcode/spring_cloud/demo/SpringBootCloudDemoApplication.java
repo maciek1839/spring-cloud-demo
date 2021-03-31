e7d54b012f4858bcbfa30cf524c962b7c6cd02e1
MaciejK <maciek1839@gmail.com>
2021-03-31 20:45:07 +0200

    Initial commit. Generate Maven project

---
+++ src/main/java/com/showmeyourcode/spring_cloud/demo/SpringBootCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.showmeyourcode.spring_cloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloudDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/showmeyourcode/spring_cloud/demo/SpringBootCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showmeyourcode.spring_cloud.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
