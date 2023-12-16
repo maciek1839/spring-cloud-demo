4ce42ceccc6f44c84ed43178ad51f44419430ec9
Maciek <maciek1839@gmail.com>
2023-12-16 13:54:07 +1100

    feat: expose data using GraphQL (reporting microservice)

--- reporting-microservice/src/main/java/com/showmeyourcode/spring_cloud/demo/reporting/constant/EndpointConstant.java
+++ reporting-microservice/src/main/java/com/showmeyourcode/spring_cloud/demo/reporting/constant/EndpointConstant.java
@@ -15,6 +15,12 @@ public class EndpointConstant {
     public static final String SHOP_MICROSERVICE = API_PREFIX + "/shop";
     public static final String FACTORY_MICROSERVICE = API_PREFIX + "/factory";
 
+    public static final String GRAPHQL_ROOT = "/graphql";
+    public static final String GRAPHQL_GRAPHIQL = "/graphiql";
+    public static final String GRAPHQL_ALTAIR = "/altair";
+    public static final String GRAPHQL_PLAYGROUND = "/playground";
+    public static final String GRAPHQL_VOYAGER = "/voyager";
+
     private EndpointConstant() {
     }
 }
